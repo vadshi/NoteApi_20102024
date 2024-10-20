cb98dd89cfbe87e24bf4d341ad5959493699aeef
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-10-20 15:05:24 +0300

    Add doc responses examples

--- api/handlers/user.py
+++ api/handlers/user.py
@@ -1,12 +1,13 @@
 from flask import jsonify
 from api import app, db, request, multi_auth
 from api.models.user import UserModel
-from api.schemas.user import user_schema, users_schema, UserSchema
+from api.schemas.user import user_schema, users_schema, UserSchema, UserRequestSchema
 from flask_apispec  import doc, marshal_with, use_kwargs
 
 
 @app.route("/users/<int:user_id>")
 @doc(description='Api for getting only user.', tags=['Users'], summary="Get user by id")
+@doc(responses={"404": {"description": "User not found"}})
 @marshal_with(UserSchema, code=200)
 def get_user_by_id(user_id):
     user = db.get_or_404(UserModel, user_id, description=f"User with id={user_id} not found")
@@ -26,10 +27,12 @@ def get_users():
 
 
 @app.route("/users", methods=["POST"])
-def create_user():
-    user_data = request.json
-    user = UserModel(**user_data)
-    # TODO: добавить обработчик на создание пользователя с неуникальным username
+@doc(description='Api to create new  user.', tags=['Users'], summary="Create new user")
+@use_kwargs(UserRequestSchema, location='json')
+def create_user(**kwargs):
+    # user_data = request.json
+    user = UserModel(**kwargs)
+    # DONE: добавить обработчик на создание пользователя с неуникальным username
     if db.session.scalars(db.select(UserModel).where(UserModel.username==user.username)).one_or_none():
         return {"error": "User already exists."}, 409
     user.save()
@@ -49,6 +52,8 @@ def edit_user(user_id):
 
 @app.route("/users/<int:user_id>", methods=["DELETE"])
 @multi_auth.login_required(role="admin")
+@doc(responses={"404": {"description": "User not found"}})
+@doc(description='Api to delete user.', tags=['Users'], summary="Delete user by id")
 def delete_user(user_id):
     """
     Пользователь может удалять ТОЛЬКО свои заметки
--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from api import app, docs
 from config import Config
 from api.handlers import user
 
-
+# Для добавления в Swagger необходимо выполнить регистрацию очередного endpoint'a
 docs.register(user.get_user_by_id)
+docs.register(user.create_user)
+docs.register(user.delete_user)
 
 
 if __name__ == '__main__':
